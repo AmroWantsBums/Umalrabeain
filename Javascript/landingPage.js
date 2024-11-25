17aeb0588918ef8fc0edd9e8dcf6d9088a88e875
Amro <2592997@students.wits.ac.za>
2024-11-25 23:28:12 +0200

    Landing Page

---
+++ Javascript/landingPage.js
@@ -0,0 +1,17 @@
+const logo = document.querySelector("#umalrabeain-logo");
+const dot = document.querySelector("#pre-load-dot");
+
+setTimeout(() => {
+    dot.style.opacity = "1";
+    setTimeout(() => {
+        logo.style.opacity = "1";
+        document.querySelector("body").style.backgroundColor = "#FFF4EC";
+        setTimeout(() => { 
+            dot.style.opacity = "0";
+            logo.style.opacity = "0";
+            setTimeout(() => {  
+                window.location.href = "./Home/homePage.html";
+            }, 1000);
+        }, 2000);
+    }, 1000);
+}, 1000);
