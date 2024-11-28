bfdbddbd8c3c17e3ab8f2c8553b71ec4ffb63a1e
Amro <2592997@students.wits.ac.za>
2024-11-28 09:34:23 +0200

    Back To CDN

--- Javascript/globe.js
+++ Javascript/globe.js
@@ -1,6 +1,3 @@
-import * as THREE from "three";
-
-
 // Create scene and camera
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
